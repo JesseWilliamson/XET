44d85fb8462d6850279fbb84c34a711003135bf6
Jesse Williamson <jnathanwilliamson@gmail.com>
2020-06-21 21:24:33 +1000

    added existing files

---
+++ renderer.js
@@ -0,0 +1,92 @@
+var app = require('electron').remote;
+var dialog = app.dialog;
+var fs = require('fs');
+// const {writeFile} = require('fs');
+// const {readFile} = require('fs');
+WIN = app.getCurrentWindow();
+
+
+
+function test1(){
+    var content = document.getElementById('content').value;
+    var contentarr = content.split(/[\s,\r]+/);
+    var titlearr
+
+
+
+    console.log(contentarr.length)
+
+    titlearr = contentarr.filter(function (item) {
+        return item.indexOf("#") == 0;
+     });
+
+    console.log(titlearr);
+
+
+
+
+
+    let ListBox = document.getElementById('ListBox');
+    let arr = ['html', 'css', 'javascript'];
+    
+    let list = "<ul>";
+    
+    for (let i = 0; i < titlearr.length; i++){
+        list += "<li>" + titlearr[i] + "</li>"
+    }
+    list += "</ul>"
+    
+    ListBox.innerHTML = list
+};
+
+function readFile(filepath){
+    fs.readFile(filepath, 'utf-8', (err, data) => {
+        if (err){
+            console.log('error')
+            return;
+        }
+        console.log(data)
+        content.value = data;
+    })
+
+}
+
+async function openFile() {
+    
+    var content = document.getElementById('content').value;
+    
+    const paths = dialog.showOpenDialogSync(WIN, { properties: ['openFile', 'multiSelections'] });
+    console.log(paths[0]);
+    readFile(paths[0])  
+    
+    
+
+}
+
+
+async function saveFile() {
+
+    var content = document.getElementById('content').value;
+
+    console.log("Save button");
+
+
+    let { filePath } = await dialog.showSaveDialog({
+        buttonlabel: 'Save file'
+    });
+    
+    console.log(filePath);
+
+    fs.writeFile(filePath, content, () => console.log('we done fam'));
+
+    console.log("saved sucessfully!");
+    console.log(content);
+
+};
+
+document.getElementById("save").onclick = saveFile;
+document.getElementById("open").onclick = openFile;
+document.getElementById("test1").onclick = test1;
+
+
+
